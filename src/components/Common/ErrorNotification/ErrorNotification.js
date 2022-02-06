ed81727112cf0b6f2433cfd0091045fd39ed6d79
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-06 23:41:02 +0200

    Implement Error notification on login

--- src/components/Common/ErrorNotification/ErrorNotification.js
+++ src/components/Common/ErrorNotification/ErrorNotification.js
@@ -1,7 +1,6 @@
 import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
 
-const ErrorNotification = () => ({
+const ErrorNotification = ({
     show,
     errMessage
 }) => {
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,16 @@
 import * as authService from '../../services/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../contexts/AuthContext';
+import ErrorNotification from
+    '../Common/ErrorNotification/ErrorNotification';
+import { useState } from 'react';
 
 
 const Login = () => {
-    const { login } = useAuthContext();
     const navigate = useNavigate();
+    const { login } = useAuthContext();
+    const [show, setShow] = useState(false);
+    const [errMessage, setErrMessage] = useState();
 
     const onLogin = async (e) => {
         e.preventDefault();
@@ -20,32 +25,41 @@ const Login = () => {
             login(data);
             navigate('/');
         } catch (err) {
-            // TODO: Show notification
-            console.log(err.message);
+            setErrMessage(err.message);
+            setShow(true);
+            setTimeout(() => {
+                setShow(false);
+            }, 1500);
         }
     };
 
     return (
-        <section id="login-page" className="login">
-            <form id="login-form" onSubmit={onLogin}>
-                <fieldset>
-                    <legend>Login Form</legend>
-                    <p className="field">
-                        <label htmlFor="email">Email</label>
-                        <span className="input">
-                            <input type="text" name="email" id="email" placeholder="Email" />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="password">Password</label>
-                        <span className="input">
-                            <input type="password" name="password" id="password" placeholder="Password" />
-                        </span>
-                    </p>
-                    <input className="button submit" type="submit" value="Login" />
-                </fieldset>
-            </form>
-        </section>
+        <>
+            <ErrorNotification
+                show={show}
+                errMessage={errMessage}
+            />
+            <section id="login-page" className="login">
+                <form id="login-form" onSubmit={onLogin}>
+                    <fieldset>
+                        <legend>Login Form</legend>
+                        <p className="field">
+                            <label htmlFor="email">Email</label>
+                            <span className="input">
+                                <input type="text" name="email" id="email" placeholder="Email" />
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="password">Password</label>
+                            <span className="input">
+                                <input type="password" name="password" id="password" placeholder="Password" />
+                            </span>
+                        </p>
+                        <input className="button submit" type="submit" value="Login" />
+                    </fieldset>
+                </form>
+            </section>
+        </>
     );
 };
 
